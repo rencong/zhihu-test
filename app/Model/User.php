dfcf3cbbe4b8a061ecb0a40351502e1a9108e5cf
renc <rc2860105819@163.com>
2018-07-31 18:09:52 +0800

    重置密码

--- app/Model/User.php
+++ app/Model/User.php
@@ -4,7 +4,8 @@ namespace App\Model;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Naux\Mail\SendCloudTemplate;
 
 class User extends Authenticatable
 {
@@ -37,4 +38,18 @@ class User extends Authenticatable
 //    {
 //        $this->attributes['password'] = Hash::make($password);
 //    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        // 模板变量
+        $bind_data = [
+            'url'  => url(config('app.url').route('password.reset', $token, false))
+        ];
+        $template = new SendCloudTemplate('password_reset', $bind_data);
+
+        Mail ::raw($template, function ($message){
+            $message->from('2860105819@qq.com', 'Renc');
+            $message->to($this->email);
+        });
+    }
 }
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,9 +14,11 @@ return [
     */
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => '密码重置成功!',
+    'sent' => '重置密码邮件已成功发送!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "没有找到对应的该邮箱信息",
 
 ];
+
+
